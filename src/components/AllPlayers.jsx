1fcc4ed16c0938a9b5f567b51ffae24def2e616f
Zachary-Collier <zachary.collier@saintleo.edu>
2023-08-07 16:00:25 -0400

    Added information

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,4 +1,22 @@
+import { useState, useEffect } from "react";
+
+
+
 function AllPlayers() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    async function PlayersFetch() {
+      try {
+        const data = await fetchPlayers();
+        return setPlayers(data);
+      } catch (error) {
+        setError(error);
+      }
+    }
+    PlayersFetch();
+  }, [handleDelete]);
+
     return (
       <div>
         <h1>All Players component</h1>
