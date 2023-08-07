799f6952dc2844b3c7a261020aaa140562c8edb1
Zachary-Collier <zachary.collier@saintleo.edu>
2023-08-07 16:10:58 -0400

    Additional needed changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import AllPlayers from "./components/AllPlayers"
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/Navbar";
+import AllPlayers from "./components/AllPlayers";
+import SinglePlayer from "./components/SinglePlayer";
+import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>App component</h1>
-    </div>
+    <>
+      {" "}
+      <main className="main">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<AllPlayers />} />
+          <Route path={`/players/:id`} element={<SinglePlayer />} />
+        </Routes>
+      </main>
+      <NewPlayerForm />
+    </>
   );
 }
-
-
